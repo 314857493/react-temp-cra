25b5a705495c357a6d019fb56b1b54aa556b7e27
jiaxingdong <jiaxingdong@jd.com>
2022-02-21 11:37:53 +0800

    feat:update 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "no-param-reassign": 0,
     "no-unused-expressions": 0,
     "react-hooks/exhaustive-deps": 0,
-    "no-console": 1,
-    "no-debugger": 2,
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
 };
